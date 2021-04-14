afebf7f18d1a2b560b5c00032e8b4ce3491e7f44
SkyZhangT <zhangtianyangsky@gmail.com>
2021-04-14 14:31:44 -0700

    some progress

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,70 @@
 package com.company;
 
-public class Main {
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        connection con = new connection("cRF2dvDZQsmu37WGgK6MTcL7XjH");
+
+        try {
+            String ret = con.Get();
+            System.out.println(ret);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
+class connection{
+
+    HttpsURLConnection con;
+    static String auth = "Y1JGMmR2RFpRc211MzdXR2dLNk1UY0w3WGpI";
+
+    public connection(String id){
+        URL url = null;
+
+        try {
+            url = new URL("https://eluv.io/items/"+id);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            this.con = (HttpsURLConnection) url.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            this.con.setRequestMethod("GET");
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        }
+
+        this.con.setRequestProperty("Authorization", auth);
+        this.con.setConnectTimeout(5000);
+    }
+
+    public String Get() throws IOException {
+        int status = this.con.getResponseCode();
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(this.con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+
+        return content.toString();
     }
 }
